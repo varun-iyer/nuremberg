257c059186413c0dbd28d5f650f20c8ad77ab02a
Varun Iyer <varun_iyer@posteo.net>
2022-08-03 10:43:44 -0400

    Add .db to database filename

--- nuremberg/settings/dev.py
+++ nuremberg/settings/dev.py
@@ -6,7 +6,7 @@ DEBUG = True
 COMPRESS_ENABLED = False
 DATABASES['default'] = {
   'ENGINE': 'django.db.backends.sqlite3',
-  'NAME': 'nuremberg_dev',
+  'NAME': 'nuremberg_dev.db',
   'USER': 'nuremberg',
   'HOST': 'localhost',
 }
